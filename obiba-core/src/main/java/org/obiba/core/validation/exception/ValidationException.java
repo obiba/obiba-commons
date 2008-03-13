f8a05af292ef0c0ef99afac7bdef0ae9b4d22e3e
Yannick Marcon <yannick.marcon@obiba.org>
2008-03-13 21:02:46 +0000

    ValidationException improved.

--- obiba-core/src/main/java/org/obiba/core/validation/exception/ValidationException.java
+++ obiba-core/src/main/java/org/obiba/core/validation/exception/ValidationException.java
@@ -1,7 +1,9 @@
 package org.obiba.core.validation.exception;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 
 public class ValidationException extends RuntimeException {
@@ -15,6 +17,30 @@ public class ValidationException extends RuntimeException {
 	public ValidationException(final List<Errors> errors) {
 		this.errors = errors;
 	}
+  
+  public ValidationException() {
+    this.errors = new LinkedList<Errors>();
+  }
+  
+  public ValidationException(Object target, String errorCode, String defaultMessage) {
+    this.errors = new LinkedList<Errors>();
+    reject(target, errorCode, defaultMessage);
+  }
+
+  public ValidationException(Object target, String errorCode, Object[] errorArgs, String defaultMessage) {
+    this.errors = new LinkedList<Errors>();
+    reject(target, errorCode, errorArgs, defaultMessage);
+  }
+  
+  public ValidationException(String errorCode, String defaultMessage) {
+    this.errors = new LinkedList<Errors>();
+    reject(null, errorCode, defaultMessage);
+  }
+  
+  public ValidationException(String errorCode, Object[] errorArgs, String defaultMessage) {
+    this.errors = new LinkedList<Errors>();
+    reject(null, errorCode, errorArgs, defaultMessage);
+  }
 
 	/**
 	 * @return Returns the errors.
@@ -29,5 +55,48 @@ public class ValidationException extends RuntimeException {
 	public void setErrors(final List<Errors> errors) {
 		this.errors = errors;
 	}
+  
+  public void reject(Object target, String errorCode) {
+    Errors error = getTargetErrors(target);
+    error.reject(errorCode);
+    errors.add(error);
+  }
+
+  public void reject(Object target, String errorCode, String defaultMessage) {
+    Errors error = getTargetErrors(target);
+    error.reject(errorCode, defaultMessage);
+    errors.add(error);
+  }
+
+  public void reject(Object target, String errorCode, Object[] errorArgs, String defaultMessage) {
+    Errors error = getTargetErrors(target);
+    error.reject(errorCode, errorArgs, defaultMessage);
+    errors.add(error);
+  }
+  
+  public void reject(String errorCode) {
+    reject(null, errorCode);
+  }
+
+  public void reject(String errorCode, String defaultMessage) {
+    reject(null, errorCode, defaultMessage);
+  }
+
+  public void reject(String errorCode, Object[] errorArgs, String defaultMessage) {
+    reject(null, errorCode, errorArgs, defaultMessage);
+  }
+  
+  private Errors getTargetErrors(Object target) {
+    for (Errors error : errors) {
+      if (error instanceof BindException) {
+        if (target != null && target.equals(((BindException)error).getTarget()))
+          return error;
+        else if (target == null && ((BindException)error).getTarget() == null)
+          return error;
+      }
+    }
+    
+    return new BindException(target, target != null ? target.getClass().getName() : "null");
+  }
 
 }
