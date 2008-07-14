6a2820f7c81eac2531626e34dd49c2c108801b42
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-07-14 21:17:39 +0000

    Added WebApplicationStartupListener for listening on startup events of Wicket web application.

---
+++ obiba-wicket/src/main/java/org/obiba/wicket/util/seed/DatabaseSeedWebApplicationStartupListener.java
@@ -0,0 +1,37 @@
+package org.obiba.wicket.util.seed;
+
+import java.util.List;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.obiba.wicket.application.WebApplicationStartupListener;
+
+/**
+ * Implements {@link WebApplicationStartupListener} to allow seeding a database with a list of {@link DatabaseSeed}
+ * instances.
+ * 
+ */
+public class DatabaseSeedWebApplicationStartupListener implements WebApplicationStartupListener {
+
+  private List<DatabaseSeed> databaseSeeds;
+
+  public void shutdown(WebApplication application) {
+    // Nothing to do.
+  }
+
+  public void startup(WebApplication application) {
+    if(databaseSeeds != null) {
+      for(DatabaseSeed seed : databaseSeeds) {
+        seed.seedDatabase(application);
+      }
+    }
+  }
+
+  public void setDatabaseSeeds(List<DatabaseSeed> databaseSeeds) {
+    this.databaseSeeds = databaseSeeds;
+  }
+
+  public List<DatabaseSeed> getDatabaseSeeds() {
+    return databaseSeeds;
+  }
+
+}
