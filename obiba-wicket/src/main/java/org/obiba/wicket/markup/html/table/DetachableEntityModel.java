f9d625a8819abebc154522317e6baa8df4ef062d
Yannick Marcon <yannick.marcon@obiba.org>
2008-09-04 17:57:22 +0000

    Case object to load is a IEntity do not query the id, just ask it to the object.

--- obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/DetachableEntityModel.java
+++ obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/DetachableEntityModel.java
@@ -3,6 +3,7 @@ package org.obiba.wicket.markup.html.table;
 import java.io.Serializable;
 
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.obiba.core.domain.IEntity;
 import org.obiba.core.service.EntityQueryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,49 +14,56 @@ import org.slf4j.LoggerFactory;
  */
 public class DetachableEntityModel extends LoadableDetachableModel {
 
-  private static final long serialVersionUID = 1606621482493529188L;
+	private static final long serialVersionUID = 1606621482493529188L;
 
-  private static final Logger log = LoggerFactory.getLogger(DetachableEntityModel.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(DetachableEntityModel.class);
 
-  private Serializable id;
+	private Serializable id;
 
-  private Class<?> type;
+	private Class<?> type;
 
-  private EntityQueryService service;
+	private EntityQueryService service;
 
-  public DetachableEntityModel(EntityQueryService service, Object o) {
-    super(o);
-    if (o == null)
-      throw new IllegalArgumentException("model object cannot be null");
-    this.service = service;
-    this.service.refresh(o);
-    this.id = service.getId(o);
-    this.type = o.getClass();
-  }
+	public DetachableEntityModel(EntityQueryService service, Object o) {
+		super(o);
+		if (o == null)
+			throw new IllegalArgumentException("model object cannot be null");
+		this.service = service;
+		this.service.refresh(o);
+		if (o instanceof IEntity)
+			this.id = ((IEntity) o).getId();
+		else
+			this.id = service.getId(o);
+		this.type = o.getClass();
+	}
 
-  @Override
-  protected void onDetach() {
-    super.onDetach();
-    log.trace("DetachableEntityModel has detached instance id {} of type {}", id, type.getSimpleName());
-  }
+	@Override
+	protected void onDetach() {
+		super.onDetach();
+		log.trace(
+				"DetachableEntityModel has detached instance id {} of type {}",
+				id, type.getSimpleName());
+	}
 
-  @Override
-  protected Object load() {
-    log.trace("DetachableEntityModel is loading instance id {} of type {}", id, type.getSimpleName());
-    return service.get(type, id);
-  }
+	@Override
+	protected Object load() {
+		log.trace("DetachableEntityModel is loading instance id {} of type {}",
+				id, type.getSimpleName());
+		return service.get(type, id);
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof DetachableEntityModel) {
-      DetachableEntityModel rhs = (DetachableEntityModel) obj;
-      return this.id.equals(rhs.id) && this.type.equals(rhs.type);
-    }
-    return super.equals(obj);
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof DetachableEntityModel) {
+			DetachableEntityModel rhs = (DetachableEntityModel) obj;
+			return this.id.equals(rhs.id) && this.type.equals(rhs.type);
+		}
+		return super.equals(obj);
+	}
 
-  @Override
-  public int hashCode() {
-    return 37 * this.id.hashCode() * this.type.hashCode();
-  }
+	@Override
+	public int hashCode() {
+		return 37 * this.id.hashCode() * this.type.hashCode();
+	}
 }
