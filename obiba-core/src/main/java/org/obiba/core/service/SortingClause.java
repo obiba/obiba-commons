cef9039cd3e79a9f717922d9b8886eead9b9a851
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-09-18 21:32:10 +0000

    SortingClause:   - Added equals and hashcode implementations.   - Added Javadoc.

--- obiba-core/src/main/java/org/obiba/core/service/SortingClause.java
+++ obiba-core/src/main/java/org/obiba/core/service/SortingClause.java
@@ -1,24 +1,53 @@
 package org.obiba.core.service;
 
+/**
+ * An instance of this class enables sorting results from {@link EntityQueryService}.
+ */
+final public class SortingClause {
 
-public class SortingClause {
-
+  /**
+   * The field the sorting clause applies to
+   */
   private String field = null;
+
+  /**
+   * Whether sorting is done ascending (default) or descending.
+   */
   private boolean ascending = true;
 
+  /**
+   * Creates an ascending <code>SortingClause</code> on the specified field.
+   * 
+   * @param field the field to sort on.
+   */
   public SortingClause(String field) {
     this(field, true);
   }
-  
+
+  /**
+   * Creates a <code>SortingClause</code> on the specified field in the specified direction.
+   * @param field the field to sort on.
+   * @param asc the direction to sort in.
+   */
   public SortingClause(String field, boolean asc) {
     this.field = field;
     this.ascending = asc;
   }
 
+  /**
+   * Creates a <code>SortingClause</code> on the specified field in the specified direction.
+   * @param field the field to sort on.
+   * @param asc the direction to sort in.
+   */
   static public SortingClause create(String field, boolean asc) {
     return new SortingClause(field, asc);
   }
 
+  /**
+   * Creates an ascending <code>SortingClause</code> on the specified field.
+   * 
+   * @param field the field to sort on.
+   */
   static public SortingClause create(String field) {
     return new SortingClause(field);
   }
@@ -30,7 +59,23 @@ public class SortingClause {
   public boolean isAscending() {
     return this.ascending;
   }
-  
+
+  @Override
+  public boolean equals(Object obj) {
+    if(obj instanceof SortingClause) {
+      SortingClause rhs = (SortingClause) obj;
+      return field.equals(rhs.field) && ascending == rhs.ascending;
+    }
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = 17;
+    result = 37 * result + field.hashCode();
+    return 37 * result + (ascending ? 0 : 1);
+  }
+
   @Override
   public String toString() {
     return "{" + field + (ascending ? " asc" : " desc") + "}";
