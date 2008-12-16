aa4988e6ca7bcb6ebf2fb151785e2ffae80dcb03
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-12-16 03:32:40 +0000

    Fixed XStream misuse: was passing resource encoding as the XML root node object.

--- obiba-wicket/src/main/java/org/obiba/wicket/util/seed/XstreamResourceDatabaseSeed.java
+++ obiba-wicket/src/main/java/org/obiba/wicket/util/seed/XstreamResourceDatabaseSeed.java
@@ -1,6 +1,7 @@
 package org.obiba.wicket.util.seed;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
@@ -17,167 +18,178 @@ import org.springframework.transaction.annotation.Transactional;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.XStreamException;
 
-public class XstreamResourceDatabaseSeed implements DatabaseSeed,
-		InitializingBean, ResourceLoaderAware {
+public class XstreamResourceDatabaseSeed implements DatabaseSeed, InitializingBean, ResourceLoaderAware {
 
-	private final Logger log = LoggerFactory
-			.getLogger(XstreamResourceDatabaseSeed.class);
+  private final Logger log = LoggerFactory.getLogger(XstreamResourceDatabaseSeed.class);
 
-	private XStream xstream = new XStream();
+  private XStream xstream = new XStream();
 
-	private Resource xstreamResource;
+  private Resource xstreamResource;
 
-	private String[] xstreamResourcePatterns;
+  private String[] xstreamResourcePatterns;
 
-	private ResourcePatternResolver resolver;
+  private ResourcePatternResolver resolver;
 
-	private Map<String, Class<?>> aliases;
+  private Map<String, Class<?>> aliases;
 
-	private String resourceEncoding = "ISO-8859-1";
+  private String resourceEncoding = "ISO-8859-1";
 
-	private boolean developmentSeed = false;
+  private boolean developmentSeed = false;
 
-	@SuppressWarnings("unchecked")
-	@Transactional
-	public void seedDatabase(WebApplication application) {
-		if (shouldSeed(application) == false) {
-			return;
-		}
+  @SuppressWarnings("unchecked")
+  @Transactional
+  public void seedDatabase(WebApplication application) {
+    if(shouldSeed(application) == false) {
+      return;
+    }
 
-		if (xstreamResource != null) {
-			try {
-				Object xstreamResult = null;
-				
-				xstreamResult = xstream.fromXML(xstreamResource
-						.getInputStream(), resourceEncoding);
-				
-				handleXstreamResult(xstreamResult);
-			} catch (UnsupportedEncodingException e) {
-				throw new RuntimeException(e);
-			} catch (IOException e) {
-				log.error("Error parsing XStream Resource.", e);
-				throw new RuntimeException(e);
-			} catch (XStreamException e) {
-				log.error("Invalid XStream Resource.", e);
-				throw e;
-			} catch (RuntimeException e) {
-				log.error("Error parsing XStream Resource.", e);
-				throw e;
-			}
-		}
+    if(xstreamResource != null) {
+      InputStreamReader reader = null;
+      try {
+        Object xstreamResult = null;
 
-		if (xstreamResourcePatterns != null) {
-			for (String locationPattern : xstreamResourcePatterns) {
-				try {
-					Resource[] resources = resolver
-							.getResources(locationPattern);
-					if (resources != null) {
-						for (Resource resource : resources) {
-							Object xstreamResult = null;
+        reader = new InputStreamReader(xstreamResource.getInputStream(), resourceEncoding);
+        xstreamResult = xstream.fromXML(new InputStreamReader(xstreamResource.getInputStream(), resourceEncoding));
 
-							xstreamResult = xstream.fromXML(resource
-									.getInputStream(), resourceEncoding);
+        handleXstreamResult(xstreamResult);
+      } catch(UnsupportedEncodingException e) {
+        throw new RuntimeException(e);
+      } catch(IOException e) {
+        log.error("Error parsing XStream Resource.", e);
+        throw new RuntimeException(e);
+      } catch(XStreamException e) {
+        log.error("Invalid XStream Resource.", e);
+        throw e;
+      } catch(RuntimeException e) {
+        log.error("Error parsing XStream Resource.", e);
+        throw e;
+      } finally {
+        if(reader != null) {
+          try {
+            reader.close();
+          } catch(Exception e) {
+            // ignore
+          }
+        }
+      }
+    }
 
-							handleXstreamResult(resource, xstreamResult);
-						}
-					}
+    if(xstreamResourcePatterns != null) {
+      for(String locationPattern : xstreamResourcePatterns) {
+        try {
+          Resource[] resources = resolver.getResources(locationPattern);
+          if(resources != null) {
+            for(Resource resource : resources) {
+              Object xstreamResult = null;
 
-				} catch (UnsupportedEncodingException e) {
-					throw new RuntimeException(e);
-				} catch (IOException e) {
-					log.error("Error parsing XStream Resource.", e);
-					throw new RuntimeException(e);
-				} catch (XStreamException e) {
-					log.error("Invalid XStream Resource.", e);
-					throw e;
-				} catch (RuntimeException e) {
-					log.error("Error parsing XStream Resource.", e);
-					throw e;
-				}
-			}
-		}
-	}
+              InputStreamReader reader = new InputStreamReader(resource.getInputStream(), resourceEncoding);
+              try {
+                xstreamResult = xstream.fromXML(reader);
+              } finally {
+                if(reader != null) {
+                  try {
+                    reader.close();
+                  } catch(Exception e) {
+                    // ignore
+                  }
+                }
+              }
 
-	public void afterPropertiesSet() throws Exception {
-		if ((this.xstreamResource == null || this.xstreamResource.exists() == false)
-				&& (this.xstreamResourcePatterns == null || this.xstreamResourcePatterns.length == 0)) {
-			log
-					.error("XStream resource not specified, no seeding will take place. Make sure the 'resource' or 'resourcePatterns' property are set.");
-		} else {
-			initializeXstream(xstream);
-		}
-	}
+              handleXstreamResult(resource, xstreamResult);
+            }
+          }
 
-	public void setResourcePatterns(String[] xstreamResourcePatterns) {
-		this.xstreamResourcePatterns = xstreamResourcePatterns;
-	}
+        } catch(UnsupportedEncodingException e) {
+          throw new RuntimeException(e);
+        } catch(IOException e) {
+          log.error("Error parsing XStream Resource.", e);
+          throw new RuntimeException(e);
+        } catch(XStreamException e) {
+          log.error("Invalid XStream Resource.", e);
+          throw e;
+        } catch(RuntimeException e) {
+          log.error("Error parsing XStream Resource.", e);
+          throw e;
+        }
+      }
+    }
+  }
 
-	public void setResource(Resource xstreamResource) {
-		this.xstreamResource = xstreamResource;
-	}
+  public void afterPropertiesSet() throws Exception {
+    if((this.xstreamResource == null || this.xstreamResource.exists() == false) && (this.xstreamResourcePatterns == null || this.xstreamResourcePatterns.length == 0)) {
+      log.error("XStream resource not specified, no seeding will take place. Make sure the 'resource' or 'resourcePatterns' property are set.");
+    } else {
+      initializeXstream(xstream);
+    }
+  }
 
-	protected Resource getResource() {
-		return xstreamResource;
-	}
+  public void setResourcePatterns(String[] xstreamResourcePatterns) {
+    this.xstreamResourcePatterns = xstreamResourcePatterns;
+  }
 
-	public void setAliases(Map<String, Class<?>> aliases) {
-		this.aliases = aliases;
-	}
+  public void setResource(Resource xstreamResource) {
+    this.xstreamResource = xstreamResource;
+  }
 
-	public void setDevelopmentSeed(boolean devSeed) {
-		this.developmentSeed = devSeed;
-	}
+  protected Resource getResource() {
+    return xstreamResource;
+  }
 
-	protected boolean shouldSeed(WebApplication application) {
-		if (developmentSeed == true) {
-			// If this is a development seed, only seed if the WebApplication
-			// was deployed
-			// in development mode
-			if (WebApplication.DEVELOPMENT.equalsIgnoreCase(application
-					.getConfigurationType()) == false) {
-				return false;
-			}
-		} else {
-			// If this is not a development seed, only seed if the
-			// WebApplication was deployed
-			// in deployment mode
-			if (WebApplication.DEPLOYMENT.equalsIgnoreCase(application
-					.getConfigurationType()) == false) {
-				return false;
-			}
-		}
-		return true;
-	}
+  public void setAliases(Map<String, Class<?>> aliases) {
+    this.aliases = aliases;
+  }
 
-	/**
-	 * Called after 'resource' has been processed.
-	 * @param result
-	 */
-	protected void handleXstreamResult(Object result) {
+  public void setDevelopmentSeed(boolean devSeed) {
+    this.developmentSeed = devSeed;
+  }
 
-	}
+  protected boolean shouldSeed(WebApplication application) {
+    if(developmentSeed == true) {
+      // If this is a development seed, only seed if the WebApplication
+      // was deployed
+      // in development mode
+      if(WebApplication.DEVELOPMENT.equalsIgnoreCase(application.getConfigurationType()) == false) {
+        return false;
+      }
+    } else {
+      // If this is not a development seed, only seed if the
+      // WebApplication was deployed
+      // in deployment mode
+      if(WebApplication.DEPLOYMENT.equalsIgnoreCase(application.getConfigurationType()) == false) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	/**
-	 * Called after one resource from 'resourcePatterns' has been processed.
-	 * @param resource
-	 * @param result
-	 */
-	protected void handleXstreamResult(Resource resource, Object result) {
+  /**
+   * Called after 'resource' has been processed.
+   * @param result
+   */
+  protected void handleXstreamResult(Object result) {
 
-	}
+  }
 
-	protected void initializeXstream(XStream xstream) {
-		xstream.setMode(XStream.ID_REFERENCES);
-		if (this.aliases != null) {
-			for (Map.Entry<String, Class<?>> aliasEntry : aliases.entrySet()) {
-				log.debug("Adding XStream alias '{}' for class {}", aliasEntry
-						.getKey(), aliasEntry.getValue());
-				xstream.alias(aliasEntry.getKey(), aliasEntry.getValue());
-			}
-		}
-	}
+  /**
+   * Called after one resource from 'resourcePatterns' has been processed.
+   * @param resource
+   * @param result
+   */
+  protected void handleXstreamResult(Resource resource, Object result) {
 
-	public void setResourceLoader(ResourceLoader resourceLoader) {
-		this.resolver = (ResourcePatternResolver) resourceLoader;
-	}
+  }
+
+  protected void initializeXstream(XStream xstream) {
+    xstream.setMode(XStream.ID_REFERENCES);
+    if(this.aliases != null) {
+      for(Map.Entry<String, Class<?>> aliasEntry : aliases.entrySet()) {
+        log.debug("Adding XStream alias '{}' for class {}", aliasEntry.getKey(), aliasEntry.getValue());
+        xstream.alias(aliasEntry.getKey(), aliasEntry.getValue());
+      }
+    }
+  }
+
+  public void setResourceLoader(ResourceLoader resourceLoader) {
+    this.resolver = (ResourcePatternResolver) resourceLoader;
+  }
 }
