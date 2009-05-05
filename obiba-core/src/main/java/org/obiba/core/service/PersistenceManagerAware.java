9ce12b98cf32947c3a9e0ee3289ce1ed6c66ea10
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-05-05 20:39:20 +0000

    Created PersistenceManagerAware interface.

---
+++ obiba-core/src/main/java/org/obiba/core/service/PersistenceManagerAware.java
@@ -0,0 +1,19 @@
+package org.obiba.core.service;
+
+/**
+ * Interface to be implemented by any object that wishes to be notified of the {@link PersistenceManager} available in
+ * the {@code ApplicationContext} that it runs in.
+ * <p>
+ * Note that it is not sufficient to implement this class in order to obtain the {@code PersistenceManager}, the
+ * appropriate &lt;property&gt; node must also be specified. Automatic injection requires a {@code
+ * BeanFactoryPostProcessor}.
+ */
+public interface PersistenceManagerAware {
+
+  /**
+   * Set the {@code PersistenceManager} available in the {@code ApplicationContext}
+   * @param persistenceManager
+   */
+  public void setPersistenceManager(PersistenceManager persistenceManager);
+
+}
--- obiba-core/src/main/java/org/obiba/core/service/impl/PersistenceManagerAwareService.java
+++ obiba-core/src/main/java/org/obiba/core/service/impl/PersistenceManagerAwareService.java
@@ -1,16 +1,19 @@
 package org.obiba.core.service.impl;
 
 import org.obiba.core.service.PersistenceManager;
+import org.obiba.core.service.PersistenceManagerAware;
 
-
-public class PersistenceManagerAwareService {
+/**
+ * Abstract helper class for services that have a dependency on the {@link PersistenceManager}.
+ */
+abstract public class PersistenceManagerAwareService implements PersistenceManagerAware {
 
   protected PersistenceManager persistenceManager;
 
   public void setPersistenceManager(PersistenceManager persistenceManager) {
     this.persistenceManager = persistenceManager;
   }
-  
+
   public PersistenceManager getPersistenceManager() {
     return persistenceManager;
   }
