b7e56da93087f803b8be7b5b69f38559b82c050b
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-05-13 15:51:31 +0000

    Modified AbstractSortableDataProvider to allow a single column to sort on multiple properties.

--- obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/AbstractSortableDataProvider.java
+++ obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/AbstractSortableDataProvider.java
@@ -1,5 +1,6 @@
 package org.obiba.wicket.markup.html.table;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,9 +10,9 @@ import org.apache.wicket.model.IModel;
 import org.obiba.core.service.PagingClause;
 import org.obiba.core.service.SortingClause;
 
-
 /**
- * Helper class that converts the {@link SortParam} to a {@link SortingClause} instance without any modification to the property name.
+ * Helper class that converts the {@link SortParam} to a {@link SortingClause} instance without any modification to the
+ * property name.
  */
 abstract public class AbstractSortableDataProvider<T> extends SortableDataProvider {
 
@@ -22,22 +23,30 @@ abstract public class AbstractSortableDataProvider<T> extends SortableDataProvid
 
   public Iterator<T> iterator(int first, int count) {
     SortParam sp = getSort();
-    SortingClause sort = null;
-    if (sp != null) {
-      sort = SortingClause.create(sp.getProperty(), sp.isAscending());
+    SortingClause[] sort = null;
+    if(sp != null) {
+      // Split on comma and whitespace
+      String[] properties = sp.getProperty().split("[,\\s]");
+      List<SortingClause> list = new ArrayList<SortingClause>(properties.length);
+      for(int i = 0; i < properties.length; i++) {
+        if(properties[i].trim().length() > 0) {
+          list.add(SortingClause.create(properties[i], sp.isAscending()));
+        }
+      }
+      sort = list.toArray(new SortingClause[0]);
     }
     return getList(PagingClause.create(first, count), sort).iterator();
   }
 
   @SuppressWarnings("unchecked")
   public IModel model(final Object object) {
-    return makeModel((T)object);
+    return makeModel((T) object);
   }
 
   abstract protected List<T> getList(PagingClause paging, SortingClause... clauses);
 
   abstract public int size();
-  
+
   abstract protected IModel makeModel(final T object);
-  
+
 }
