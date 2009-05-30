06dcbce75538064dc4a5bbd0cf7615544f47b6ee
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-05-30 02:00:11 +0000

    Fixed logging API usage.

--- obiba-core/src/main/java/org/obiba/core/validation/interceptor/MethodArgValidationInterceptor.java
+++ obiba-core/src/main/java/org/obiba/core/validation/interceptor/MethodArgValidationInterceptor.java
@@ -5,35 +5,34 @@ import java.util.List;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.obiba.core.validation.exception.ValidationRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.Errors;
 
 public class MethodArgValidationInterceptor extends ObjectValidationInspector implements MethodInterceptor {
-	
-	private Log log = LogFactory.getLog(getClass());
-  
-	/**
-	 * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
-	 */
-	public Object invoke(final MethodInvocation methodInvocation)
-			throws Throwable {
-    log.info("method=" + methodInvocation.getMethod().getName());
-		final List<Errors> errors = new ArrayList<Errors>();
-		for (int i = 0; i < methodInvocation.getArguments().length; i++) {
-			final Object arg = methodInvocation.getArguments()[i];
-      log.info("method=" + methodInvocation.getMethod().getName() + " object=" + arg.getClass().getName());
-			// Inspect fields... of this arg.
-			inspectObject(errors, arg);
-		}
 
-		if (errors.size() > 0) {
-			log.warn("Validation error(s) found, throwing ValidationException.");
-			throw new ValidationRuntimeException(errors);
-		}
+  private final static Logger log = LoggerFactory.getLogger(MethodArgValidationInterceptor.class);
 
-		return methodInvocation.proceed();
-	}
+  /**
+   * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+   */
+  public Object invoke(final MethodInvocation methodInvocation) throws Throwable {
+    log.info("method={}", methodInvocation.getMethod().getName());
+    final List<Errors> errors = new ArrayList<Errors>();
+    for(int i = 0; i < methodInvocation.getArguments().length; i++) {
+      final Object arg = methodInvocation.getArguments()[i];
+      log.info("method={} object={}", methodInvocation.getMethod().getName(), arg.getClass().getName());
+      // Inspect fields... of this arg.
+      inspectObject(errors, arg);
+    }
+
+    if(errors.size() > 0) {
+      log.warn("Validation error(s) found, throwing ValidationException.");
+      throw new ValidationRuntimeException(errors);
+    }
+
+    return methodInvocation.proceed();
+  }
 
 }
--- obiba-core/src/main/java/org/obiba/core/validation/interceptor/ObjectValidationInspector.java
+++ obiba-core/src/main/java/org/obiba/core/validation/interceptor/ObjectValidationInspector.java
@@ -17,22 +17,22 @@ import java.util.TreeSet;
 import java.util.Vector;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.obiba.core.service.EntityQueryService;
 import org.obiba.core.validation.validator.AbstractPersistenceAwareClassValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 public class ObjectValidationInspector {
-  
+
+  private final static Logger log = LoggerFactory.getLogger(ObjectValidationInspector.class);
+
   private List<Validator> validators = new ArrayList<Validator>();
 
   protected EntityQueryService entityQueryService;
-  
-  private Log log = LogFactory.getLog(getClass());
-  
+
   /**
    * @return Returns the validators.
    */
@@ -41,95 +41,86 @@ public class ObjectValidationInspector {
   }
 
   /**
-   * @param validators
-   *          The validators to set.
+   * @param validators The validators to set.
    */
   public void setValidators(List<Validator> validators) {
     this.validators = validators;
   }
-  
+
   public void setEntityQueryService(EntityQueryService entityQueryService) {
     this.entityQueryService = entityQueryService;
   }
-  
+
   public EntityQueryService getEntityQueryService() {
     return entityQueryService;
   }
 
-  private void inspectObjectProperties(final Object arg,
-      final List<Errors> errors) {
+  private void inspectObjectProperties(final Object arg, final List<Errors> errors) {
     // Inspect supported properties.
-    final PropertyDescriptor[] propertyDescriptors = PropertyUtils
-        .getPropertyDescriptors(arg.getClass());
-    for (final PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+    final PropertyDescriptor[] propertyDescriptors = PropertyUtils.getPropertyDescriptors(arg.getClass());
+    for(final PropertyDescriptor propertyDescriptor : propertyDescriptors) {
       Object propertyValue;
       try {
-        propertyValue = propertyDescriptor.getReadMethod().invoke(
-            arg);
-        if (propertyValue != null
-            && isArrayOrCollection(propertyValue.getClass())) {
-          if (propertyValue.getClass().isArray()) {
-            for (final Object propertyElementValue : ((Object[]) propertyValue)) {
-              for (final Validator validator : getValidators()) {
-                if (validator.supports(propertyElementValue.getClass())) {
-                  log.debug("Validator supported: "
-                      + propertyElementValue.getClass());
+        propertyValue = propertyDescriptor.getReadMethod().invoke(arg);
+        if(propertyValue != null && isArrayOrCollection(propertyValue.getClass())) {
+          if(propertyValue.getClass().isArray()) {
+            for(final Object propertyElementValue : ((Object[]) propertyValue)) {
+              for(final Validator validator : getValidators()) {
+                if(validator.supports(propertyElementValue.getClass())) {
+                  log.debug("Validator supported: " + propertyElementValue.getClass());
                   validateAndAddErrors(propertyElementValue, validator, errors);
                   inspectObjectProperties(propertyElementValue, errors);
                 }
               }
             }
           } else {
-            for (final Object propertyElementValue : ((Collection) propertyValue)) {
-              for (final Validator validator : getValidators()) {
-                if (validator.supports(propertyElementValue.getClass())) {
-                  log.debug("Validator supported: "
-                      + propertyElementValue.getClass());
+            for(final Object propertyElementValue : ((Collection<?>) propertyValue)) {
+              for(final Validator validator : getValidators()) {
+                if(validator.supports(propertyElementValue.getClass())) {
+                  log.debug("Validator supported: " + propertyElementValue.getClass());
                   validateAndAddErrors(propertyElementValue, validator, errors);
                   inspectObjectProperties(propertyElementValue, errors);
                 }
               }
             }
           }
-        } else if (propertyValue != null) {
+        } else if(propertyValue != null) {
           // Non-Scalar property
-          for (final Validator validator : getValidators()) {
-            if (validator.supports(propertyValue.getClass())) {
+          for(final Validator validator : getValidators()) {
+            if(validator.supports(propertyValue.getClass())) {
               log.debug("Validator supported: " + propertyValue.getClass());
               validateAndAddErrors(propertyValue, validator, errors);
               inspectObjectProperties(propertyValue, errors);
             }
           }
         }
-      } catch (IllegalArgumentException e) {
+      } catch(IllegalArgumentException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
-      } catch (IllegalAccessException e) {
+      } catch(IllegalAccessException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
-      } catch (InvocationTargetException e) {
+      } catch(InvocationTargetException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
     }
   }
 
-  private Errors validateAndAddErrors(final Object arg,
-      final Validator validator, final List<Errors> errors) {
-    final BindException objErrors = new BindException(arg, arg.getClass()
-        .getName());
+  private Errors validateAndAddErrors(final Object arg, final Validator validator, final List<Errors> errors) {
+    final BindException objErrors = new BindException(arg, arg.getClass().getName());
     validator.validate(arg, objErrors);
-    if (objErrors.hasErrors()) {
+    if(objErrors.hasErrors()) {
       errors.add(objErrors);
     }
     return objErrors;
   }
 
   public void inspectObject(final List<Errors> errors, final Object arg) {
-    for (final Validator validator : getValidators()) {
-      if (validator.supports(arg.getClass())) {
-        if (entityQueryService != null && validator instanceof AbstractPersistenceAwareClassValidator) {
-          ((AbstractPersistenceAwareClassValidator)validator).setEntityQueryService(entityQueryService);
+    for(final Validator validator : getValidators()) {
+      if(validator.supports(arg.getClass())) {
+        if(entityQueryService != null && validator instanceof AbstractPersistenceAwareClassValidator) {
+          ((AbstractPersistenceAwareClassValidator) validator).setEntityQueryService(entityQueryService);
         }
         log.debug("Validator supported: " + arg.getClass());
         validateAndAddErrors(arg, validator, errors);
@@ -138,18 +129,7 @@ public class ObjectValidationInspector {
     }
   }
 
-  private boolean isArrayOrCollection(final Class clazz) {
-    return (clazz.isArray() || clazz.isAssignableFrom(List.class)
-        || clazz.isAssignableFrom(ArrayList.class)
-        || clazz.isAssignableFrom(Set.class)
-        || clazz.isAssignableFrom(SortedSet.class)
-        || clazz.isAssignableFrom(AbstractCollection.class)
-        || clazz.isAssignableFrom(AbstractList.class)
-        || clazz.isAssignableFrom(AbstractSet.class)
-        || clazz.isAssignableFrom(HashSet.class)
-        || clazz.isAssignableFrom(LinkedHashSet.class)
-        || clazz.isAssignableFrom(LinkedList.class)
-        || clazz.isAssignableFrom(TreeSet.class) || clazz
-        .isAssignableFrom(Vector.class));
+  private boolean isArrayOrCollection(final Class<?> clazz) {
+    return (clazz.isArray() || clazz.isAssignableFrom(List.class) || clazz.isAssignableFrom(ArrayList.class) || clazz.isAssignableFrom(Set.class) || clazz.isAssignableFrom(SortedSet.class) || clazz.isAssignableFrom(AbstractCollection.class) || clazz.isAssignableFrom(AbstractList.class) || clazz.isAssignableFrom(AbstractSet.class) || clazz.isAssignableFrom(HashSet.class) || clazz.isAssignableFrom(LinkedHashSet.class) || clazz.isAssignableFrom(LinkedList.class) || clazz.isAssignableFrom(TreeSet.class) || clazz.isAssignableFrom(Vector.class));
   }
 }
