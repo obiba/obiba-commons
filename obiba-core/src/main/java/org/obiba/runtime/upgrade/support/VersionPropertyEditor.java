523313df024f7708b4a59c94da4f523c0da31c0b
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-08-03 20:32:01 +0000

    OPAL-29: Refactoring: Moved common upgrade management code from onyx-core to obiba-core.

---
+++ obiba-core/src/main/java/org/obiba/runtime/upgrade/support/VersionPropertyEditor.java
@@ -0,0 +1,18 @@
+package org.obiba.runtime.upgrade.support;
+
+import java.beans.PropertyEditorSupport;
+
+import org.obiba.runtime.Version;
+
+public class VersionPropertyEditor extends PropertyEditorSupport {
+  //
+  // PropertyEditorSupport Methods
+  //
+  
+  @Override
+  public void setAsText(String versionString)
+  {
+    Version version = new Version(versionString);
+    setValue(version);
+  }
+}
