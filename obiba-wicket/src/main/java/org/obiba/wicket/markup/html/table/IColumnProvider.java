707a2f57ad9c4e0fba87792056c64b5368c21b5e
David Bujold <david.bujold@mail.mcgill.ca>
2009-11-04 22:41:22 +0000

    Commented on the generic's meaning for classes implementing this.

--- obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/IColumnProvider.java
+++ obiba-wicket/src/main/java/org/obiba/wicket/markup/html/table/IColumnProvider.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 
+/**
+ * A column provider will provide the columns to be displayed for an EntityListTable.
+ *
+ * @param <T> the class of the entity that will be represented in the generated table.
+ */
 public interface IColumnProvider<T> {
 
   public List<String> getColumnHeaderNames();
