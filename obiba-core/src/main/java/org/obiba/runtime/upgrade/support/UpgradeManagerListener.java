30bfb9c220da1152ad0edc8d3260de037a350c23
Cedric Thiebault <cthiebault@p3g.org>
2013-08-14 14:58:34 -0400

    Fix warnings

--- obiba-core/src/main/java/org/obiba/runtime/upgrade/support/UpgradeManagerListener.java
+++ obiba-core/src/main/java/org/obiba/runtime/upgrade/support/UpgradeManagerListener.java
@@ -5,15 +5,15 @@ import org.obiba.runtime.upgrade.UpgradeStep;
 
 public interface UpgradeManagerListener {
 
-  public void onBeforeStep(InstallStep step);
+  void onBeforeStep(InstallStep step);
 
-  public void onAfterStep(InstallStep step);
+  void onAfterStep(InstallStep step);
 
-  public void onFailedStep(InstallStep step, Exception e);
+  void onFailedStep(InstallStep step, Exception e);
 
-  public void onBeforeStep(UpgradeStep step);
+  void onBeforeStep(UpgradeStep step);
 
-  public void onAfterStep(UpgradeStep step);
+  void onAfterStep(UpgradeStep step);
 
-  public void onFailedStep(UpgradeStep step, Exception e);
+  void onFailedStep(UpgradeStep step, Exception e);
 }
