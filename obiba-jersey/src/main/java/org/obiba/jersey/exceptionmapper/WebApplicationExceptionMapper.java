dfac913f4f3edda158876b08c425e50f1743cb4b
Cedric Thiebault <cthiebault@p3g.org>
2014-04-08 11:52:01 -0400

    Add exception mappers

---
+++ obiba-jersey/src/main/java/org/obiba/jersey/exceptionmapper/WebApplicationExceptionMapper.java
@@ -0,0 +1,24 @@
+package org.obiba.jersey.exceptionmapper;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+
+  private static final Logger log = LoggerFactory.getLogger(WebApplicationExceptionMapper.class);
+
+  @Override
+  public Response toResponse(WebApplicationException exception) {
+    //TODO use ErrorDto
+    log.error("WebApplicationException", exception);
+    return exception.getResponse();
+  }
+
+}
+
