edb3625b0be944a66bd9f0a550dcc74ca4059aad
Cedric Fontin <cfontin@maelstrom-research.org>
2016-12-12 17:24:49 -0500

    moved PrefixedValueTranslator from mica2

---
+++ obiba-core/src/test/java/org/obiba/core/translator/PrefixedValueTranslatorTest.java
@@ -0,0 +1,46 @@
+package org.obiba.core.translator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PrefixedValueTranslatorTest {
+
+  @Test
+  public void when_non_prefixed_value__return_the_same_value() throws Exception {
+
+    assertThat(prefixedSimpleTranslator().translate("toTranslate"), is("toTranslate"));
+    assertThat(prefixedSimpleTranslator().translate("nonExistent"), is("nonExistent"));
+  }
+
+  @Test
+  public void when_prefixed_value__return_translated_value() throws Exception {
+    assertThat(prefixedSimpleTranslator().translate("t(toTranslate)"), is("translated"));
+  }
+
+  @Test
+  public void when_complete_sentence_with_many_prefixes__return_translated_sentence() throws Exception {
+    assertThat(prefixedSimpleTranslator().translate("Phrase with some words t(toTranslate) and t(toTranslate2)"),
+        is("Phrase with some words translated and translated2"));
+  }
+
+  private Translator prefixedSimpleTranslator() {
+    return new PrefixedValueTranslator(new SimpleTranslator());
+  }
+
+  private class SimpleTranslator implements Translator {
+
+    @Override
+    public String translate(String key) {
+      switch (key) {
+        case "toTranslate":
+          return "translated";
+        case "toTranslate2":
+          return "translated2";
+        default:
+          return key;
+      }
+    }
+  }
+}
