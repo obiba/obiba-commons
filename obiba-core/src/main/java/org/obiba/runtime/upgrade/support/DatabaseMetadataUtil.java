b093d62eb8e9a657fefe7aa0f81b565f4a465cb6
Yannick Marcon <yannick.marcon@obiba.org>
2016-12-18 19:01:40 +0100

    Update copyright

--- obiba-core/src/main/java/org/obiba/runtime/upgrade/support/DatabaseMetadataUtil.java
+++ obiba-core/src/main/java/org/obiba/runtime/upgrade/support/DatabaseMetadataUtil.java
@@ -1,11 +1,12 @@
-/***********************************************************************************************************************
- * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+/*
+ * Copyright (c) 2016 OBiBa. All rights reserved.
  *
- * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- **********************************************************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.obiba.runtime.upgrade.support;
 
 import java.sql.DatabaseMetaData;
